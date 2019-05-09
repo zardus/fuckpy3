9ce0ece0f520dcd3ff0695d4b7286917b83da74f
Yan <yans@yancomm.net>
2019-05-09 11:47:19 -0700

    setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='fuckpy3',
+    version='1.0',
+    py_modules=['fuckpy3'],
+    description='Some much-needed sanity for byte-wrangling in python3.',
+    url='https://github.com/zardus/fuckpy3',
+)
